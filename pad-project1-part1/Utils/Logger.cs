8a6e9d3b6fc141d8721eb574c775eecb34e98327
aiordan <ana-maria.iordan@endava.com>
2020-09-06 18:34:57 +0300

    add sender and utils solutions

---
+++ pad-project1-part1/Utils/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Utils
+{
+    public static class Logger
+    {
+        private static TextWriter textWriter;
+
+        public static void Log(string logMessage)
+        {
+            using(StreamWriter streamWriter = File.AppendText("log.txt"))
+            {
+                textWriter.Write("\r\nLog Entry: ");
+                textWriter.Write($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+                textWriter.Write(" :");
+                textWriter.Write($" :{logMessage}");
+                textWriter.Write("-------------------------------");
+            }
+        }
+    }
+}
