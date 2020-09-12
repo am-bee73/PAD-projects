ccedb1242b3acc9f71ac3b9d3955a2c0939612d2
Rotaru <rotaruandrian5698@gmail.com>
2020-09-12 18:26:12 +0300

    Receiver bug

---
+++ pad-project1-part1/Receiver/PayloadHandler.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Utils;
+
+namespace Receiver
+{
+    class PayloadHandler
+    {
+        public static void Handle(byte[] payloadBytes)
+        {
+            var payloadString = Encoding.UTF8.GetString(payloadBytes);
+            var payload = JsonConvert.DeserializeObject<Data
+                >(payloadString);
+
+            Console.WriteLine(payload.Subject);
+        }
+    }
+}
